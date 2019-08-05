b46d69743a9d07c9683dd9ac3f0f0b5b63ced892
Jessy Diamond Exum <jexum@meraki.com>
2019-08-04 20:37:30 -0700

    Added basic simulator and serial network adapter.

---
+++ python/src/att26a/serial_adapter.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python3
+
+import logging
+import socket
+import sys
+import time
+import threading
+import serial
+import serial.rfc2217
+import queue
+
+from att26a import interruptablequeue
+
+class RFC2217SerialAdapter(object):
+    class FakePort(object):
+        def __init__(self, realport):
+            self.realport = realport
+
+        @property
+        def cts(self):
+            return self.realport._cts
+        @cts.setter
+        def cts(self, value):
+            self.realport._cts = value
+
+        @property
+        def dsr(self):
+            return self.realport._dsr
+        @dsr.setter
+        def dsr(self, value):
+            self.realport._dsr = value
+
+        @property
+        def ri(self):
+            return self.realport._ri
+        @ri.setter
+        def ri(self, value):
+            self.realport._ri = value
+
+        @property
+        def cd(self):
+            return self.realport._cd
+        @cd.setter
+        def cd(self, value):
+            self.realport._cd = value
+
+        @property
+        def baudrate(self):
+            return self.realport._baudrate
+        @baudrate.setter
+        def baudrate(self, value):
+            self.realport._baudrate = value
+
+        @property
+        def bytesize(self):
+            return self.realport._bytesize
+        @bytesize.setter
+        def bytesize(self, value):
+            self.realport._bytesize = value
+
+        @property
+        def parity(self):
+            return self.realport._parity
+        @parity.setter
+        def parity(self, value):
+            self.realport._parity = value
+
+        @property
+        def stopbits(self):
+            return self.realport._stopbits
+        @stopbits.setter
+        def stopbits(self, value):
+            self.realport._stopbits = value
+
+        @property
+        def xonxoff(self):
+            return self.realport._xonxoff
+        @xonxoff.setter
+        def xonxoff(self, value):
+            self.realport._xonxoff = value
+
+        @property
+        def rtscts(self):
+            return self.realport._rtscts
+        @rtscts.setter
+        def rtscts(self, value):
+            self.realport._rtscts = value
+
+        @property
+        def break_condition(self):
+            return self.realport._break_condition
+        @break_condition.setter
+        def break_condition(self, value):
+            self.realport._break_condition = value
+
+        @property
+        def dtr(self):
+            return self.realport._dtr
+        @dtr.setter
+        def dtr(self, value):
+            self.realport._dtr = value
+
+        @property
+        def rts(self):
+            return self.realport._rts
+        @rts.setter
+        def rts(self, value):
+            self.realport._rts = value
+
+        def reset_input_buffer(self):
+            pass
+        def reset_output_buffer(self):
+            pass
+
+    class FakeConnection(object):
+        def __init__(self, realconn):
+            self.realconn = realconn
+
+        def write(self, data):
+            """thread safe socket write with no data escaping. used to send telnet stuff"""
+            with self.realconn._write_lock:
+                if self.realconn.socket:
+                    self.realconn.socket.sendall(data)
+
+    def __init__(self, address="", port=7778, debug=False):
+        self._address = address
+        self._port = port
+        self._debug = debug
+        self._is_open = False
+
+        self._cts = False
+        self._dsr = False
+        self._ri = False
+        self._cd = False
+        self._baudrate = False
+        self._bytesize = False
+        self._parity = False
+        self._stopbits = False
+        self._xonxoff = False
+        self._rtscts = False
+        self._break_condition = False
+        self._dtr = False
+        self._rts = False
+
+        self.connection_alive = False
+        self.socket = None
+        self._write_lock = threading.Lock()
+        self.rfc2217 = None
+        self.__reader = interruptablequeue.InterruptableQueue()
+        self.log = logging.getLogger('RFC2217SerialAdapter')
+
+        self.thread_server = None
+
+        self.open()
+
+    def open(self):
+        if not self._is_open:
+            self._is_open = True
+            self.thread_server = threading.Thread(
+                target=self.server_thread, daemon=True
+            )
+            self.thread_server.start()
+
+    def close(self):
+        if self._is_open:
+            self._is_open = False
+
+            if self.socket: # Break out of socket recv()
+                self.socket.shutdown(socket.SHUT_RDWR)
+            else:           # Break out of server accept()
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    sock.connect((self._address, self._port))
+
+            # Sync up threads
+            self.thread_server.join()
+
+    def server_thread(self):
+        srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        srv.bind((self._address, self._port))
+        srv.listen(1)
+        logging.info("TCP/IP port: {}".format(self._port))
+
+        while self._is_open:
+            try:
+                self.socket, addr = srv.accept()
+                if not self._is_open:
+                    print("breaking out of accept")
+                    break
+                logging.info('Connected by {}:{}'.format(addr[0], addr[1]))
+                self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                self.rfc2217 = serial.rfc2217.PortManager(
+                    RFC2217SerialAdapter.FakePort(self),
+                    RFC2217SerialAdapter.FakeConnection(self),
+                    logger=logging.getLogger('rfc2217.server') if self._debug else None
+                )
+                self.connection_alive = True
+
+                try:
+                    while self.connection_alive:
+                        try:
+                            data = self.socket.recv(1024)
+                            if not data:
+                                print("Breaking out of recv")
+                                break
+                            data_in = b''.join(self.rfc2217.filter(data))
+                            for b in data_in:
+                                self.__reader.put(b)
+                        except socket.error as msg:
+                            self.log.error('{}'.format(msg))
+                            break
+                finally:
+                    logging.info('Disconnected')
+                    self.connection_alive = True
+                    self.socket.close()
+                    self.socket = None
+            except socket.error as msg:
+                logging.error(str(msg))
+
+        print("server_thread dead")
+
+    def write(self, data):
+        if not self._is_open: raise Exception("Closed")
+        with self._write_lock:
+            if self.socket:
+                # escape outgoing data when needed (Telnet IAC (0xff) character)
+                self.socket.sendall(b''.join(self.rfc2217.escape(data)))
+
+    def read(self, length):
+        if not self._is_open: raise Exception("Closed")
+        if length != 1:
+            raise ValueError("multi byte read does not work yet")
+        while True:
+            try:
+                return bytes([self.__reader.get(block=True, timeout=0.3)])
+            except queue.Empty as e:
+                if not self._is_open:
+                    raise Exception("Closed")
+
+    @property
+    def cts(self):
+        return self._cts
+    @cts.setter
+    def cts(self, value):
+        self._cts = value
+        self.rfc2217.check_modem_lines()
+
+    @property
+    def dsr(self):
+        return self._dsr
+    @dsr.setter
+    def dsr(self, value):
+        self._dsr = value
+        self.rfc2217.check_modem_lines()
+
+    @property
+    def ri(self):
+        return self._ri
+    @ri.setter
+    def ri(self, value):
+        self._ri = value
+        self.rfc2217.check_modem_lines()
+
+    @property
+    def cd(self):
+        return self._cd
+    @cd.setter
+    def cd(self, value):
+        self._cd = value
+        self.rfc2217.check_modem_lines()
+
+    @property
+    def baudrate(self):
+        return self._baudrate
+    @baudrate.setter
+    def baudrate(self, value):
+        self._baudrate = value
+
+    @property
+    def bytesize(self):
+        return self._bytesize
+
+    @property
+    def parity(self):
+        return self._parity
+
+    @property
+    def stopbits(self):
+        return self._stopbits
+
+    @property
+    def xonxoff(self):
+        return self._xonxoff
+
+    @property
+    def rtscts(self):
+        return self._rtscts
+
+    @property
+    def break_condition(self):
+        return self._break_condition
+
+    @property
+    def dtr(self):
+        return self._dtr
+
+    @property
+    def rts(self):
+        return self._rts
+
+    def reset_input_buffer(self):
+        pass
+    def reset_output_buffer(self):
+        pass
---
+++ python/src/att26a/simulator.py
@@ -0,0 +1,254 @@
+import collections
+import threading
+import math
+import time
+
+LED_OFF = 0x0
+LED_BLINK1 = 0x8
+LED_BLINK2 = 0xD
+LED_ON = 0xF
+
+LED_MODES = (LED_OFF, LED_BLINK1, LED_BLINK2, LED_ON)
+
+MSG_KA = 0xFF # Keep Alive
+MSG_ACK = 0xFD # Acknowledge
+
+class Att26aSimBase(object):
+    def __init__(self, serialdev):
+        self.__ser = serialdev
+        self.__recvbuff = collections.deque(maxlen=16)
+
+        self.__do_recvthread = False
+        self.__recvthread = None
+
+        self.__do_keepalivethread = False
+        self.__keepalivethread = None
+
+        self.reset()
+
+    def reset(self):
+        # Terminate the reader thread
+        self.__do_recvthread = False
+        if self.__recvthread is not None:
+            self.__recvthread.wait(0.5)
+
+        # (Re)start the reader thread
+        self.__do_recvthread = True
+        self.__recvthread = threading.Thread(daemon=True, target=self.__recvthread_func)
+        self.__recvthread.start()
+
+        # Terminate the writer thread
+        self.__do_keepalivethread = False
+        if self.__keepalivethread is not None:
+            self.__keepalivethread.wait(0.5)
+
+        # (Re)start the reader thread
+        self.__do_keepalivethread = True
+        self.__keepalivethread = threading.Thread(
+            daemon=True, target=self.__keepalivethread_func)
+        self.__keepalivethread.start()
+
+    def __recvthread_func(self):
+        retdata = bytearray()
+        while self.__do_recvthread:
+            try:
+                data = self.__ser.read(1) # Blocks
+            except serial.serialutil.SerialException as e:
+                if self._verbose:
+                    print("ATT26A closing due to exception on receiver thread: '%s'" % e)
+                self._close(dojoin=False)
+
+            self._rx(data)
+
+    def __keepalivethread_func(self):
+        print("Simulator Keepalive Message Thread STARTING")
+        while self.__do_keepalivethread:
+            self.__ser.write(b'\xFF')
+            time.sleep(0.026)
+
+        print("Simulator Keepalive Message Thread TERMINATING")
+
+    @staticmethod
+    def _shift7_left(b):
+        return ((b << 1) & 0x7E) | ((b & 0x40) >> 6)
+
+    @staticmethod
+    def _shift7_right(b):
+        return ((b & 0x7E) >> 1) | ((b & 0x01) << 6)
+
+    @staticmethod
+    def _check_msg(msg):
+        h = 0x7F
+        for b in msg[1:-1]:
+            h ^= b
+        #print("Expecting %x"%h)
+        return h == msg[-1]
+
+    def send_btn_press(self, btn_id):
+        self.__ser.write(bytes((Att26aSimBase._shift7_left(btn_id),)))
+
+    def _rx(self, data):
+        for b in data:
+            #print("RECV", b)
+            self._rx_byte(b)
+
+    def _rx_byte(self, b):
+        if 0 > b > 255: raise ValueError("Invalid byte value")
+        if b == 0xFF:
+            if len(self.__recvbuff) >= 2:
+                msg = b"".join(self.__recvbuff)
+                if not Att26aSimBase._check_msg(msg):
+                    print("Message failed verification, DROP!")
+                else:
+                    self._msg_dispatch(msg[:-1])
+        elif b == 0x85 or b == 0xA5:
+            self.__recvbuff.clear()
+            self.__recvbuff.append(b.to_bytes(1, 'little'))
+        else:
+            self.__recvbuff.append(b.to_bytes(1, 'little'))
+
+    def _msg_dispatch(self, msg):
+        print("Message:", msg)
+        if len(msg) < 3:
+            self._tx_ack()
+            return
+        msgcat, msgtype, msgparam = msg[0], msg[1], msg[2:]
+
+        if msgcat == 0x85: # WRITE
+            print("Commamd type is WRITE")
+            if msgtype == 0x07: # Set LED range ON/OFF (0-99)
+                print("TRYING TO DO LED RANGE", msgparam, len(msgparam))
+                if len(msgparam) >= 3:
+                    print("Still trying")
+                    led_id = Att26aSimBase._shift7_right(msgparam[0])
+                    led_count = msgparam[1]
+                    led_data = msgparam[2:]
+                    print(led_id, led_count, led_data)
+                    print("CHECKING", 0 <= led_id <= 99,
+                          (1 <= led_count <= 70 or 72 <= led_count <= 76),
+                          math.ceil(led_count/7.00) == len(led_data))
+                    if 0 <= led_id <= 99 and \
+                       (1 <= led_count <= 70 or 72 <= led_count <= 76) and \
+                       math.ceil(led_count/7.00) == len(led_data):
+                        state_array = []
+                        for d in led_data:
+                            if d & 0x80:
+                                print("Invalid set led range data byte %x"%d)
+                                break
+                            for bit in range(6,-1,-1):
+                                print("LEDCOUNT", led_count)
+                                if led_count == -1: break
+                                led_count -= 1;
+                                state_array.append(bool((d >> bit) & 1))
+                        else:
+                            self.on_set_led_range_state(led_id, state_array)
+            elif (msgtype & 0xF0) == 0x20: # Set LED state (0-119)
+                led_state = (msgtype & 0x0F)
+                led_id = Att26aSimBase._shift7_right(msgparam[0])
+                if led_state in LED_MODES and 0 <= led_id <= 119:
+                    self.on_set_led_state(led_state, led_id)
+            elif msgtype == 0x10 and \
+                 msgparam[0] == 0x6F: # Enable factory test
+                self.on_set_factory_test_mode_enable(True)
+            elif msgtype == 0x30 and \
+                msgparam[0] == 0x4F: # Disable factory test
+                self.on_set_factory_test_mode_enable(False)
+            elif msgtype == 0x40 and \
+                msgparam[0] == 0x3F: # Enable IO driver
+                self.on_set_IO_enable(True)
+            elif msgtype == 0x50 and \
+                msgparam[0] == 0x2F: # Disable IO driver
+                self.on_set_IO_enable(False)
+        elif msgcat == 0xA5: # READ
+            if msgtype == 0x20: # Return state of LED (100-119)
+                led_id = Att26aSimBase._shift7_right(msgparam[0])
+                if 100 <= led_id <= 119:
+                    led_state = self.on_get_led_status(led_id) & 0x03
+                    need_2nd_byte = led_id > 107
+                    print("Need 2nd byte: %s"%\
+                          ("YES" if need_2nd_byte else "NO"))
+                    print("LED STATE:", led_state)
+                    data_out = bytes(0x80 | (led_state << 4) | \
+                                     (need_2nd_byte << 3) |\
+                                     (0 if need_2nd_byte else ((led_id-100) & 0x07)))
+                    if need_2nd_byte:
+                        data_out += bytes(0x80 | ((led_id - 100) & 0x1F))
+
+                    data_out += bytes((MSG_ACK,))
+
+                    self.__ser.write(data_out)
+                    return
+            else:
+                print("UNKNOWN COMMAND CATEGORY")
+
+        self._tx_ack()
+
+    def _tx_ack(self):
+        self.__ser.write(b'\xFD')
+
+
+    def on_set_led_range_state(self, start_ledid, states_on_off):
+        print("Setting led range starting at %d:" % start_ledid, states_on_off)
+
+    def on_set_led_state(self, state, ledID):
+        print("Setting led %d's state to %d"%(ledID, state))
+
+    def on_set_factory_test_mode_enable(self, enable):
+        print("%s factory test" % "Enable" if enable else "Disable")
+
+    def on_set_IO_enable(self, enable):
+        print("%s IO driver" % "Enable" if enable else "Disable")
+
+    def on_get_led_status(self, ledID):
+        print("Reading led %d state" % ledID)
+        return False
+
+
+class Att26aSim(Att26aSimBase):
+    def __init__(self, serialdev):
+        super().__init__(serialdev)
+
+        self.__ledstates = [LED_MODES.index(LED_OFF)]*120
+        self._factory_test = False
+        self._io_enabled = True
+
+    def on_set_led_range_state(self, start_ledid, states_on_off):
+        print("Setting led range starting at %d: (%d)" % (start_ledid, len(states_on_off)),
+              states_on_off)
+
+    def on_set_led_state(self, state, ledID):
+        print("Setting led %d's state to %d"%(ledID, state))
+        self.__ledstates[ledID] = LED_MODES.index(state)
+
+    def on_set_factory_test_mode_enable(self, enable):
+        print("%s factory test" % "Enable" if enable else "Disable")
+        self._factory_test = enable
+
+    def on_set_IO_enable(self, enable):
+        print("%s IO driver" % "Enable" if enable else "Disable")
+        self._io_enabled = enable
+
+    def on_get_led_status(self, ledID):
+        print("Reading led %d state" % ledID)
+        return self.__ledstates[ledID]
+
+class Att26aSimEventTester(Att26aSimBase):
+    def __init__(self, serialdev):
+        super().__init__(serialdev)
+        self._events = []
+
+    def on_set_led_range_state(self, start_ledid, states_on_off):
+        self._events.append(("set_led_range_state", start_ledid, states_on_off))
+
+    def on_set_led_state(self, state, ledID):
+        self._events.append(("set_led_state", state, ledID))
+
+    def on_set_factory_test_mode_enable(self, enable):
+        self._events.append(("set_factory_test_mode_enable", enable))
+
+    def on_set_IO_enable(self, enable):
+        self._events.append(("set_IO_enable", enable))
+
+    def on_get_led_status(self, ledID):
+        self._events.append(("get_led_status", enable))
+        return False
